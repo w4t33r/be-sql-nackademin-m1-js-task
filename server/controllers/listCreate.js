5b10956fbf35c2fd4fc38139dac6daff2ff20c58
w44t3r <vk.educate@protonmail.ch>
2023-04-04 01:47:27 +0200

    List query && body validation, joi.

--- server/controllers/listCreate.js
+++ server/controllers/listCreate.js
@@ -1,5 +1,4 @@
 const db = require("../db/db")
-const jwt = require("jsonwebtoken");
 const path = require("path");
 const {listCreateSchema, updateListSchema, friendDeleteSchema} = require("../validation/validation");
 require("dotenv").config({
@@ -9,19 +8,9 @@ require("dotenv").config({
 
 module.exports.getList = async (req, res) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token) {
-            return res.status(401).json({message: 'Bad token'})
-        }
-
-        const decodedJwt = jwt.decode(token, {completed: true})
-
-        req.user = decodedJwt
-        const id = decodedJwt.id
-
+        // fetched from middleware, token verif and user Data.
+        const id = req.user.id
         const getList = "SELECT list.id, todo from list, users where fk_user = users.id and users.id =?";
-
-
         db.execute(getList, [id], (error, result) => {
             if (error) {
                 res.status(500).json({message: 'Internal server error'})
@@ -30,7 +19,7 @@ module.exports.getList = async (req, res) => {
             }
         });
     } catch (e) {
-        res.send(e)
+        res.status(500).json({message: 'Internal server error'})
     }
 
 }
@@ -38,37 +27,29 @@ module.exports.getList = async (req, res) => {
 
 module.exports.createList = (req, res) => {
     try {
-
-
         const validation = listCreateSchema.validate(req.body);
         if (validation.error) {
             return res.status(400).json(validation.error.details[0].message);
-        }
-        const {todo} = validation.value
-
-        if (todo === undefined) {
-            res.status(400).json({message: 'Bad Request'})
-        }
-
-
-        const secretKey = process.env.secret_key
-        const token = req.headers.authorization.split(' ')[1]
-        const decode = jwt.verify(token, secretKey)
-        req.user = decode
-        // const decodeToken = jwt.decode(token, {complete:true})
-        //console.log(decodeToken)
-        const id = decode.id;
-
-        const createList = "INSERT INTO list (fk_user, todo) VALUES (? , ?)";
-        db.execute(createList, [id, todo], (err, result) => {
-            if (err) {
-                res.status(500).json({message: 'Internal server error'})
+        } else {
+            //fetched from middleware, verif userdata
+            const id = req.user.id
+            const {todo} = validation.value
+            if (todo === undefined) {
+                res.status(400).json({message: 'Bad Request'})
             } else {
-                res.status(201).json({message: 'Created'})
+                const createList = "INSERT INTO list (fk_user, todo) VALUES (? , ?)";
+                db.execute(createList, [id, todo], (err, result) => {
+                    if (err) {
+                        res.status(500).json({message: 'Internal server error'})
+                    } else {
+                        res.status(201).json({message: 'Created'})
+                    }
+                })
             }
-        })
+
+        }
     } catch (e) {
-        res.status('Server Error')
+        res.status(500).json({message: 'Internal server error'})
     }
 }
 
@@ -79,24 +60,48 @@ module.exports.updateList = async (req, res) => {
         const validation = updateListSchema.validate(req.body);
         if (validation.error) {
             return res.status(400).json(validation.error.details[0].message);
-        }
-        const {id, todo} = validation.value
-
-        if (todo === undefined || id === undefined) {
-            res.status(400).json({message: 'Bad Request'})
-        }
-        const updateList = "UPDATE list SET todo = ? WHERE id = ?";
-        db.execute(updateList, [todo, id], (err, result) => {
-            if (err) {
-                res.status(500).json({message: 'Internal server error'})
+        } else {
+            const {id, todo} = validation.value
+            const userId = req.user.id
+            if (todo === undefined || id === undefined) {
+                res.status(400).json({message: 'Bad Request'})
             } else {
-                res.status(201).json({message: 'Created'})
-            }
-        })
+                const checkSql = 'SELECT fk_user, users.id, users.username, list.todo FROM list, users WHERE list.fk_user = users.id AND list.id =?'
+                const updateList = "UPDATE list SET todo = ? WHERE id = ?";
 
+                db.execute(checkSql, [id], (err, result) => {
+                    if (err) {
+                        res.status(500).json({message: 'Internal server error'})
+                    } else if (!result.length) {
+                        res.status(401).json({message: 'Access Denied, you cant modify todos if its not yours.'})
+                    } else {
+                        const updateId = id
+                        if (result.length > 0) {
+                            result.map((async data => {
+                                const fKey = data.fk_user
+                                if (fKey === userId) {
+                                    await db.execute(updateList, [todo, updateId], (err, result) => {
+                                        if (err) {
+                                            res.status(500).json({message: 'Internal server error'})
+                                        } else {
+                                            res.status(200).json({message: 'Updated'})
+                                        }
+                                    })
+                                } else {
+                                    res.status(401).json({message: 'Access Denied, you cant modify todos if its not yours.'})
+                                }
+                            }))
+                        }
+
+                    }
+                })
+
+            }
+
+        }
 
     } catch (e) {
-        res.status('Server Error')
+        res.status(500).json({message: 'Internal server error'})
     }
 
 }
@@ -104,55 +109,50 @@ module.exports.updateList = async (req, res) => {
 
 module.exports.deleteList = async (req, res) => {
     try {
-
         const validation = friendDeleteSchema.validate(req.body);
         if (validation.error) {
             return res.status(400).json(validation.error.details[0].message);
-        }
-        const {id} = validation.value
+        } else {
+            const {id} = validation.value
+            const userId = req.user.id;
 
-        if (id === undefined) {
-            res.status(400).json({message: 'Bad Request'})
-        }
+            if (id === undefined) {
+                res.status(400).json({message: 'Bad Request'})
 
-        const secretKey = process.env.secret_key
-        const token = req.headers.authorization.split(' ')[1]
-        const decode = jwt.verify(token, secretKey)
-        req.user = decode
-        const userId = decode.id;
-
-
-        const checkSql = 'SELECT fk_user, users.id, users.username, list.todo FROM list, users WHERE list.fk_user = users.id AND list.id =?';
-        const deleteList = "DELETE FROM list WHERE id = ?";
-
-        await db.execute(checkSql, [id], async (err, result) => {
-            if (err) {
-                res.status(500).json({message: 'Internal server error'})
-            }
-            if (!result.length) {
-                res.status(401).json({message: 'Access Denied, you cant delete todos if its not yours.'})
             } else {
-                const deleteId = id
-                if (result.length > 0) {
-                    result.map((async data => {
-                        const fKey = data.fk_user
-                        if (fKey === userId) {
-                            await db.execute(deleteList, [deleteId], (err, result) => {
-                                if (err) {
-                                    res.status(500).json({message: 'Internal server error'})
-                                } else {
-                                    res.status(200).json({message: 'Deleted'})
-                                }
-                            })
+                const checkSql = 'SELECT fk_user, users.id, users.username, list.todo FROM list, users WHERE list.fk_user = users.id AND list.id =?';
+                const deleteList = "DELETE FROM list WHERE id = ?";
+                await db.execute(checkSql, [id], async (err, result) => {
+                    if (err) {
+                        res.status(500).json({message: 'Internal server error'})
+                    }
+                    if (!result.length) {
+                        res.status(401).json({message: 'Access Denied, you cant delete todos if its not yours.'})
+                    } else {
+                        const deleteId = id
+                        if (result.length > 0) {
+                            result.map((async data => {
+                                const fKey = data.fk_user
+                                if (fKey === userId) {
+                                    await db.execute(deleteList, [deleteId], (err, result) => {
+                                        if (err) {
+                                            res.status(500).json({message: 'Internal server error'})
+                                        } else {
+                                            res.status(200).json({message: 'Deleted'})
+                                        }
+                                    })
 
-                        } else {
-                            res.status(401).json({message: "Access denied"})
+                                } else {
+                                    res.status(401).json({message: "Access denied"})
+                                }
+                            }))
                         }
-                    }))
-                }
+
+                    }
+                })
 
             }
-        })
+        }
 
     } catch (e) {
         res.status(500).json({message: 'Internal server error'})
