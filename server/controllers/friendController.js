24d80e01a6928f46ac7a3a7cce82196643cf3e20
w44t3r <vk.educate@protonmail.ch>
2023-04-03 20:58:54 +0200

    removed the extra code with the token, get information from the middleware about the state of the token and the user.

--- server/controllers/friendController.js
+++ server/controllers/friendController.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const db = require("../db/db");
 const path = require("path");
 const {friendListSchema, showFriendListSchema, friendDeleteSchema} = require("../validation/validation")
+
 require("dotenv").config({
     path: path.resolve(__dirname, '../db/.env')
 });
@@ -11,7 +12,6 @@ module.exports.getFriend = async (req, res) => {
     try {
         const validation = friendListSchema.validate(req.body);
         if (validation.error) {
-            console.log(validation.error)
             return res.status(400).json(validation.error.details[0].message);
         }
         const {friendId} = validation.value
@@ -25,11 +25,9 @@ module.exports.getFriend = async (req, res) => {
             })
         }
 
-        const secretKey = process.env.secret_key
-        const token = req.headers.authorization.split(' ')[1]
-        const decode = jwt.verify(token, secretKey)
-        req.user = decode
-        const id = decode.id;
+
+        // fetched from middleware, token verif and user Data.
+        const id = req.user.id;
 
         const createList = "INSERT INTO friend (fk_users, fk_friend) VALUES (? , ?)";
         db.execute(createList, [id, friendId], (err, result) => {
@@ -48,8 +46,10 @@ module.exports.getFriend = async (req, res) => {
 module.exports.showUsers =
     async (req, res) => {
         try {
-            const showUsers = "SELECT id, username FROM users";
-            db.execute(showUsers, (err, result) => {
+            // fetched from middleware, token verif and user Data.
+            const userId = req.user.id;
+            const showUsers = "SELECT id, username FROM users where id !=?";
+            db.execute(showUsers, [userId], (err, result) => {
                 if (err) {
                     res.status(500).json({message: 'Internal server error'})
                 } else {
@@ -64,14 +64,8 @@ module.exports.showUsers =
 
 module.exports.showFriend = async (req, res) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token) {
-            return res.status(401).json({message: 'Bad token'})
-        }
-        const decodedJwt = jwt.decode(token, {completed: true})
-        req.user = decodedJwt
-        const id = decodedJwt.id
-
+        // fetched from middleware, token verif and user Data.
+        const id = req.user.id;
         const getList = "SELECT friend.id, u.username\n" +
             "from friend\n" +
             "         INNER JOIN users\n" +
@@ -136,11 +130,8 @@ module.exports.deleteFriend = async (req, res) => {
             res.status(400).json({message: 'Bad Request'})
         }
 
-        const secretKey = process.env.secret_key
-        const token = req.headers.authorization.split(' ')[1]
-        const decode = jwt.verify(token, secretKey)
-        req.user = decode
-        const userId = decode.id;
+        // fetched from middleware, token verif and user Data.
+        const userId = req.user.id;
 
 
         const checkSql = 'SELECT fk_users, fk_friend, users.id, users.username FROM friend, users WHERE friend.fk_users = users.id AND friend.id =?';
