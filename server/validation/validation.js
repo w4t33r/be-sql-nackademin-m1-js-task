5df8b934162b583da6fb5f85dbfaefb29d8cde6d
w44t3r <vk.educate@protonmail.ch>
2023-04-03 18:52:43 +0200

    added deleteFriend schema

--- server/validation/validation.js
+++ server/validation/validation.js
@@ -13,7 +13,15 @@ const friendListSchema = joi.object({
     id: joi.number().min(1)
 });
 
+// deleteFriend controller
+const friendDeleteSchema = joi.object({
+    userId: joi.number().min(1),
+    id: joi.number().min(1).required()
+});
+
+
 module.exports = {
     showFriendListSchema,
-    friendListSchema
+    friendListSchema,
+    friendDeleteSchema
 }
